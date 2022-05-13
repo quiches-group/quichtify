684e12cc2a65df569c26004c6a99c24d7ac193a4
Guillaume Chx <guillaume.chateauroux@gmail.com>
2022-05-13 15:23:37 +0200

    feat(format-number): added component and example

---
+++ src/components/FormatNumber/Example.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <q-format-number :value="1456347349.435345" :max-fraction-digits="4" locale="fr-FR" />
+    <q-format-number :value="2456347349.435345" :max-fraction-digits="2" />
+    <q-format-number :value="3456347349.435345" :max-fraction-digits="7" />
+    <q-format-number :value="4456347349.435345" />
+  </div>
+</template>
+
+<script>
+import QFormatNumber from '@/components/FormatNumber/FormatNumber.vue';
+
+export default {
+  name: 'QFormatNumberExample',
+  components: { QFormatNumber },
+};
+</script>
