89d03aa8b705e29cbba1b11d2f5198f3e3b486a0
Maxence Mottard <maxence.mottard@worldline.com>
2022-05-30 16:28:12 +0200

    create example vue

---
+++ src/components/Modal/Example.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <modal :is-open="true">
+      <p>Bonjour</p>
+    </modal>
+  </div>
+</template>
+
+<script>
+import Modal from './Modal.vue';
+
+export default {
+  name: 'Example',
+  components: { Modal },
+};
+</script>
--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -5,14 +5,7 @@
     </div>
   </div>
   <div id="open-modal" class="modal-window">
-    <div>
-      <a href="#" title="Close" class="modal-close">Close</a>
-      <h1>{{ title }}</h1>
-      <div> {{first_text }}</div>
-      <br>
-      <div><small>Check out 👇</small></div>
-      <a :href="button_link" target="_blank">{{name_link}}</a>
-    </div>
+    <slot/>
   </div>
 </template>
 
@@ -20,24 +13,11 @@
 import './Modal.scss';
 
 export default {
-  // eslint-disable-next-line vue/multi-word-component-names
   name: 'Modal',
   props: {
-    title: {
-      type: String,
-      default: 'Enter your title modal here.',
-    },
-    first_text: {
-      type: String,
-      default: 'Write your text under the title here',
-    },
-    button_link: {
-      type: String,
-      default: 'https://www.linkedin.com/in/antoine-mousset-55312417a/',
-    },
-    name_link: {
-      type: String,
-      default: 'Click her !',
+    isOpen: {
+      type: Boolean,
+      required: true,
     },
   },
   data() {},
