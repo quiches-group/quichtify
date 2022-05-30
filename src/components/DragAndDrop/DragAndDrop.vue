ed531fb1d809265c520be33d5a408a49e87cf789
LucaSardellitti <30352123+LucaSardellitti@users.noreply.github.com>
2022-05-30 16:39:12 +0200

    feat: add js function and refacto d&d

--- src/components/DragAndDrop/DragAndDrop.vue
+++ src/components/DragAndDrop/DragAndDrop.vue
@@ -1,169 +1,67 @@
 <template>
   <v-row>
-    <!-- Task -->
-      <v-row>
-        <h2 class="orange_personalize--text font-weight-thin">Tâches</h2>
-      </v-row>
-      <v-row>
-        <!-- To do task -->
-        <v-col class="col-md-5 ml-8">
-          <h3 class="title_task font-weight-thin">A faire</h3>
-          <div class="content_list">
+    <v-content>
+      <v-container fluid class="wrapper">
+        <v-row>
+          <v-col>
             <drop-list
-              :items="toDo"
+              :items="items"
               class="list"
-              @insert="onInsert($event, 'toDo')"
-              @reorder="$event.apply(toDo)"
+              @insert="onInsert($event, 'items')"
+              @reorder="$event.apply(items)"
               mode="cut"
             >
               <template v-slot:item="{item}">
                 <drag
-                  class="item pt-5"
-                  @cut="remove(toDo, item)"
+                  class="item"
+                  :class="{ 'selected' : selected.indexOf(item) > -1 }"
+                  @click="toggleSelected(items, item)"
+                  @cut="remove(items, item)"
                   :data="selection(item)"
-                  :key="item.content"
-                >
-                  <div class="content_note_text">
-                    <p>{{ item.content }}</p>
-                    <div class="btn_action_note_text">
-                      <v-btn
-                        class="btn_set"
-                        x-small
-                        fab
-                        color="transparent"
-                        elevation="0"
-                        @click="setFieldToUpdate(item.id, item.content, item.is_done)"
-                        @click.stop="dialog_update = true"
-                      >
-                        <v-icon
-                          color="black" dense>
-                          mdi-pencil
-                        </v-icon>
-                      </v-btn>
-                      <v-btn
-                        class="btn_delete"
-                        x-small
-                        fab
-                        color="transparent"
-                        elevation="0"
-                        @click="remove_task(item.id, item.is_done)">
-                        <v-icon
-                          color="black" dense>
-                          mdi-delete-outline
-                        </v-icon>
-                      </v-btn>
-                    </div>
-                  </div>
-                </drag>
+                  :key="item.name"
+                >{{item.name}}</drag>
               </template>
               <template v-slot:feedback="{data}">
                 <template v-if="selected.length > 0">
-                  <div
-                    v-for="f in data"
-                    class="item feedback" :key="f.content">{{ f.content }}
-                  </div>
+                  <div v-for="f in data" class="item feedback" :key="f.name">{{f.name}}</div>
                 </template>
                 <template v-else>
-                  <div class="item feedback" :key="data.content">{{ data.content }}</div>
+                  <div class="item feedback" :key="data.name">{{data.name}}</div>
                 </template>
               </template>
             </drop-list>
-
-            <!-- Add Task -->
-            <v-hover v-slot="{ hover }">
-              <v-card
-                max-width="185"
-                max-height="200"
-                class="mx-auto add_task"
-                :elevation="hover ? 6 : 2"
-                :class="{ 'on-hover': hover }"
-                @click.stop="dialog_task = true"
-              >
-                <v-card-text class="pa-0">
-                  <v-list class="add_task_text_content">
-                    <v-list-item>
-                      <v-img
-                        width="14px"
-                        src='@/assets/icons/plus.png'>
-                      </v-img>
-                      <v-list-item-title class="white--text ml-2 text_add_task">
-                        Ajouter une tâche
-                      </v-list-item-title>
-                    </v-list-item>
-                  </v-list>
-                </v-card-text>
-              </v-card>
-            </v-hover>
-          </div>
-        </v-col>
-
-      <!-- Done task -->
-      <v-col class="col-md-5 ml-8">
-        <h3 class="title_task font-weight-thin">Finis</h3>
-        <div class="content_list">
-          <drop-list
-            :items="done"
-            class="list"
-            @insert="onInsert($event, 'done')"
-            @reorder="$event.apply(done)"
-            mode="cut"
-          >
-            <template v-slot:item="{item}">
-              <drag
-                class="item pt-5"
-                @cut="remove(done, item)"
-                :data="selection(item)"
-                :key="item.content"
-              >
-                <div class="content_note_text">
-                  <p>{{ item.content }}</p>
-                  <div class="btn_action_note_text">
-                    <v-btn
-                      class="btn_set"
-                      x-small
-                      fab
-                      color="transparent"
-                      elevation="0"
-                      @click="setFieldToUpdate(item.id, item.content, item.is_done)"
-                      @click.stop="dialog_update = true"
-                    >
-                      <v-icon
-                        color="black" dense>
-                        mdi-pencil
-                      </v-icon>
-                    </v-btn>
-                    <v-btn
-                      class="btn_delete"
-                      x-small
-                      fab
-                      color="transparent"
-                      elevation="0"
-                      @click="remove_task(item.id, item.is_done)">
-                      <v-icon
-                        color="black" dense>
-                        mdi-delete-outline
-                      </v-icon>
-                    </v-btn>
-                  </div>
-                </div>
-              </drag>
-            </template>
-            <template v-slot:feedback="{data}">
-              <template v-if="selected.length > 0">
-                <div
-                  v-for="f in data"
-                  class="item feedback" :key="f.content">
-                  {{ f.content }}
-                </div>
+          </v-col>
+          <v-col>
+            <drop-list
+              :items="items2"
+              class="list"
+              @insert="onInsert($event, 'items2')"
+              @reorder="$event.apply(items2)"
+              mode="cut"
+            >
+              <template v-slot:item="{item}">
+                <drag
+                  class="item"
+                  :class="{ 'selected' : selected.indexOf(item) > -1 }"
+                  @cut="remove(items2, item)"
+                  @click="toggleSelected(items2, item)"
+                  :data="selection(item)"
+                  :key="item.name"
+                >{{item.name}}</drag>
               </template>
-              <template v-else>
-                <div class="item feedback" :key="data.content">{{ data.content }}</div>
+              <template v-slot:feedback="{data}">
+                <template v-if="selected.length > 0">
+                  <div v-for="f in data" class="item feedback" :key="f.name">{{f.name}}</div>
+                </template>
+                <template v-else>
+                  <div class="item feedback" :key="data.name">{{data.name}}</div>
+                </template>
               </template>
-            </template>
-          </drop-list>
-        </div>
-      </v-col>
-    </v-row>
+            </drop-list>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
   </v-row>
 </template>
 
@@ -175,7 +73,76 @@ export default {
   components: {
     Drag,
     DropList,
-  }
+  },
+  data() {
+    return {
+      items: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+        },
+        {
+          name: 'More ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+        },
+      ],
+      items2: [],
+      selected: [],
+      selectedList: 0,
+    };
+  },
+  methods: {
+    selection(item) {
+      return this.selected.length > 0 ? this.selected : item;
+    },
+    onInsert(event, listName = 'items') {
+      if (event.data.length > 0) {
+        event.data.forEach((e, idx) => {
+          this[listName].splice(event.index + idx, 0, e);
+        });
+      } else {
+        this[listName].splice(event.index, 0, event.data);
+      }
+
+      this.selected = [];
+    },
+    remove(array, value) {
+      if (this.selected.length > 0) {
+        this.selected.forEach((e) => {
+          const index = array.indexOf(e);
+          array.splice(index, 1);
+        });
+      } else {
+        const index = array.indexOf(value);
+        array.splice(index, 1);
+      }
+    },
+    toggleSelected(listName, item) {
+      if (listName !== this.selectedList) {
+        this.selected = [];
+        this.selectedList = listName;
+      }
+
+      const index = this.selected.indexOf(item);
+      if (index > -1) {
+        this.selected.splice(index, 1);
+      } else {
+        this.selected.push(item);
+      }
+    },
+  },
 };
 
 </script>
