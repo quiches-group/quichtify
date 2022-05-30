f04aa137d2e826adfadc50f8438c12942a7dbbe7
LucaSardellitti <30352123+LucaSardellitti@users.noreply.github.com>
2022-05-30 17:54:35 +0200

    feat: test drag and drop

---
+++ src/components/DragAndDrop/Example.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <DragAndDrop></DragAndDrop>
+  </div>
+</template>
+
+<script>
+import DragAndDrop from './DragAndDrop.vue';
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Example',
+  components: { DragAndDrop },
+};
+</script>
