52f40e947552e0f2eaa8436d75bee5790de9e1c4
LucaSardellitti <30352123+LucaSardellitti@users.noreply.github.com>
2022-05-31 12:00:26 +0200

    feat: bind value between components

--- src/components/DragAndDrop/Example.vue
+++ src/components/DragAndDrop/Example.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <DragAndDrop></DragAndDrop>
+    <DragAndDrop :item="data"></DragAndDrop>
   </div>
 </template>
 
@@ -11,5 +11,14 @@ export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Example',
   components: { DragAndDrop },
+  data() {
+    return {
+      data: [
+        { id: 1, title: 'Titre 1' },
+        { id: 2, title: 'Titre 2' },
+        { id: 3, title: 'Titre 3' },
+      ],
+    };
+  },
 };
 </script>
