6c0b4dde031fbd80d79ecd3f1d7d22483a2a452b
Nicolas Barbosa <nicolas.barbosa@orange.com>
2022-05-31 16:51:08 +0200

    Refactor QInput component

---
+++ src/components/QInput/QInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <input type="text" v-bind:placeholder="placeholder" v-bind:value="value">
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      required: false
+    },
+    value: {
+      type: String,
+      required: false
+    },
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import QHelloWorld from './QHelloWorld/QHelloWorld.vue';
 import QTabs from './QTabs/QTabs.vue';
-import QTab from './QTabs/QTab.vue';
 import QLoader from './QLoader/QLoader.vue';
+import QTab from './QTabs/QTab.vue';
+import QInput from './QInput/QInput.vue';
 // TODO: 2 - Import your main component here
 
 export default {
@@ -10,6 +11,7 @@ export default {
     app.component('QTabs', QTabs);
     app.component('QTab', QTab);
     app.component('QLoader', QLoader);
+    app.component('QInput', QInput);
     // TODO: 3 - Declare your component here
   },
 };
