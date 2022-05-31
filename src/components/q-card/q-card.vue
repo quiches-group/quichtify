8995eff2341d7ad2530aacc6cef3d8a12cd839fe
hugo <hugo.bordais@ynov.com>
2022-05-31 16:57:06 +0200

    feat: first modal version

---
+++ src/components/q-card/q-card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="card-container">
+    <div class="heading">
+      <h1 class="card-title">{{cardTitle}}</h1>
+      <p class="card-subtitle">{{cardSubtitle}}</p>
+    </div>
+    <p class="card-description">{{cardDescription}}</p>
+    <button class="card-btn">Confirm</button>
+    <button class="card-btn">Cancel</button>
+  </div>
+</template>
+p
+<script>
+import './q-card.scss';
+
+export default {
+  name: 'q-card',
+  props: {
+    cardTitle: String,
+    cardSubtitle: String,
+    cardDescription: String,
+  },
+};
+</script>
