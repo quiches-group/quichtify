590ba3cfa041c40789ac8a0ab4b0e9626e775d23
Florian Berrotµ <florian.berrot@ynov.com>
2022-05-31 10:45:05 +0200

    feat: finish carousel

--- src/components/Carousel/CarouselVue.vue
+++ src/components/Carousel/CarouselVue.vue
@@ -3,9 +3,13 @@
     <VueperSlide
         v-for="(slide, i) in slides"
         :key="i"
-        :image="slide.image"
         :title="slide.title"
-        :content="slide.content" />
+        :content="slide.content"
+        :style="'background-color: ' + slide.backgroundColor"/>
+    <template #loader>
+      <i class="icon icon-loader spinning"></i>
+      <span>Loading...</span>
+    </template>
   </VueperSlides>
 </template>
 
@@ -16,24 +20,15 @@ import 'vueperslides/dist/vueperslides.css';
 export default {
   name: 'CarouselVue',
   components: { VueperSlides, VueperSlide },
+  props: {
+    slides: {
+      type: Array,
+    },
+  },
   data: () => ({
     pauseOnHover: true,
     autoPlaying: true,
     internalAutoPlaying: true,
-    slides: [
-      {
-        id: 'slide-1',
-        title: 'Slide <b class="carouselTitle">#1</b>',
-        content: 'Slide title can be HTML.<br>And so does the slide content, <span class="carouselText">why not?</span>',
-        image: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.le-dictionnaire.com%2Fpaysage-campagne.jpg&imgrefurl=https%3A%2F%2Fwww.le-dictionnaire.com%2Fdefinition%2Fpaysage&tbnid=dZUQqMZrvfEfpM&vet=12ahUKEwiNl9XFpYn4AhV1YvEDHWL1B1MQMygFegUIARDjAQ..i&docid=X4Jz74B70oN_lM&w=2229&h=1000&q=paysage&client=firefox-b-d&ved=2ahUKEwiNl9XFpYn4AhV1YvEDHWL1B1MQMygFegUIARDjAQ',
-      },
-      {
-        id: 'slide-2',
-        title: 'Slide <b class="carouselTitle">#2</b>',
-        content: 'Ok bro ? <span class="carouselText">why not?</span>',
-        image: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.le-dictionnaire.com%2Fpaysage-campagne.jpg&imgrefurl=https%3A%2F%2Fwww.le-dictionnaire.com%2Fdefinition%2Fpaysage&tbnid=dZUQqMZrvfEfpM&vet=12ahUKEwiNl9XFpYn4AhV1YvEDHWL1B1MQMygFegUIARDjAQ..i&docid=X4Jz74B70oN_lM&w=2229&h=1000&q=paysage&client=firefox-b-d&ved=2ahUKEwiNl9XFpYn4AhV1YvEDHWL1B1MQMygFegUIARDjAQ',
-      },
-    ],
   }),
 };
 </script>
--- src/components/Carousel/Example.vue
+++ src/components/Carousel/Example.vue
@@ -1,5 +1,5 @@
 <template>
-  <CarouselVue/>
+  <CarouselVue :slides="slides"/>
 </template>
 
 <script>
@@ -7,7 +7,22 @@ import CarouselVue from './CarouselVue.vue';
 
 export default {
   data() {
-    return {};
+    return {
+      slides: [
+        {
+          id: 'slide-1',
+          title: 'Slide <b class="carouselTitle">#1</b>',
+          content: 'Slide title can be HTML.<br>And so does the slide content, <span class="carouselText">why not?</span>',
+          backgroundColor: '#ff5252',
+        },
+        {
+          id: 'slide-2',
+          title: 'Slide <b class="carouselTitle">#2</b>',
+          content: 'Ok bro ? <span class="carouselText">why not?</span>',
+          backgroundColor: '#42b983',
+        },
+      ],
+    };
   },
   name: 'ExampleVue',
   components: { CarouselVue },
