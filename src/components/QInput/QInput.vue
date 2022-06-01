5ba44300f1768a8f39f165cc3a37af2dfe14c96b
Nicolas Barbosa <nicolas.barbosa@orange.com>
2022-06-01 10:10:57 +0200

    Add error text

--- src/components/QInput/QInput.vue
+++ src/components/QInput/QInput.vue
@@ -1,5 +1,8 @@
 <template>
-  <input type="text" v-bind:placeholder="placeholder" v-bind:value="value">
+  <div class="aligner">
+    <input class="q-input" type="text" v-bind:placeholder="placeholder" v-bind:value="value">
+    <p class="q-input-error" v-if="showError">{{ error }}</p>
+  </div>
 </template>
 
 <script>
@@ -13,9 +16,36 @@ export default {
       type: String,
       required: false
     },
+    error: {
+      type: String,
+      required: false
+    }
+  },
+
+  computed: {
+    showError: function () {
+      return this.error !== "";
+    }
   }
 }
 </script>
 
-<style scoped>
+<style>
+.aligner {
+  @apply
+    content-start
+}
+
+.q-input {
+  @apply
+    rounded-md
+    border-4 border-transparent border-b-indigo-500
+    focus:shadow-none focus:outline-none;
+}
+
+.q-input-error {
+  @apply
+    text-red-700
+    text-xs
+}
 </style>
