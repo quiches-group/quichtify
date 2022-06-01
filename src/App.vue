24afe7ea0bdd3d06bd96329e9ccebc00b1d61a5d
Maxence Mottard <maxence.mottard@worldline.com>
2022-06-01 14:47:18 +0200

    create example for QHelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
-  <main class="flex flex-col items-center py-5">
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <q-hello-world class="mt-4" msg="Welcome on Quichify demo !" />
-    <hr class="my-10 w-1/3 h-px bg-black border-none" />
-
-    <!-- TODO: 4 - Test your component here -->
-  </main>
+  <q-hello-world-example />
 </template>
+
+<script setup>
+import QHelloWorldExample from './components/QHelloWorld/Example.vue';
+</script>
---
+++ src/components/QHelloWorld/Example.vue
@@ -0,0 +1,3 @@
+<template>
+  <q-hello-world class="mt-4" msg="Welcome on Quichify demo !" />
+</template>
