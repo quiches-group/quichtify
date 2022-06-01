9628897202940136d58952c1e55472c925947c87
Zakarya Tolba <zakarya.tolba@ynov.com>
2022-06-01 10:26:48 +0200

    Added basic drop down component from Tailwind UI

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import QHelloWorld from './QHelloWorld/QHelloWorld.vue';
 import QTabs from './QTabs/QTabs.vue';
 import QTab from './QTabs/QTab.vue';
 import QLoader from './QLoader/QLoader.vue';
+import QSelect from '@/components/QSelect/QSelect.vue';
 // TODO: 2 - Import your main component here
 
 export default {
@@ -10,6 +11,7 @@ export default {
     app.component('QTabs', QTabs);
     app.component('QTab', QTab);
     app.component('QLoader', QLoader);
+    app.component('QSelect', QSelect);
     // TODO: 3 - Declare your component here
   },
 };
