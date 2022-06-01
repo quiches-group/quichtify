f8b8efddf03c9351fda62037b4bc806857b04e2b
Cannonbark <cannonbark@pop-os.localdomain>
2022-06-01 10:24:33 +0200

    WIP: Creating sort

--- src/components/QTable/QTable.vue
+++ src/components/QTable/QTable.vue
@@ -2,9 +2,11 @@
   <table>
     <tr v-if="headers">
       <th
-          v-for="header in headers"
+          v-for="header
+          in headers"
           :key="header.value"
-          :class="`text-${header.align}`">
+          :class="`text-${header.align}`"
+          @click="sortCol(header.value, items)">
         {{ header.text }}
       </th>
     </tr>
@@ -17,15 +19,36 @@
 </template>
 
 <script setup>
+import {computed, ref} from 'vue';
+
 defineProps({
   headers: {
     type: Object,
     default: null,
   },
   items: {
-    type: String,
+    type: Object,
     default: null,
     required: true,
   },
 });
+
+const sortAsc = ref(true);
+const currentSort = ref('name');
+
+const sort = (header) => {
+  if (header === this.currentSort) {
+    this.sortAsc = !this.sortAsc;
+  }
+  this.sortAsc = true;
+  this.currentSort = header;
+};
+
+const sortCol = computed(() => this.items.sort((a, b) => {
+  let modifier = 1;
+  if (!this.sortAsc) modifier = -1;
+  if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+  if (a[this.currentSort] > b[this.currentSort]) return modifier;
+  return 0;
+}),);
 </script>
