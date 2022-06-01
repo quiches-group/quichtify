d3231271da02a7804527e1f77ad564db710005a3
Cannonbark <cannonbark@pop-os.localdomain>
2022-06-01 17:04:07 +0200

    WIP: filter and sort

--- src/components/QTable/QTable.vue
+++ src/components/QTable/QTable.vue
@@ -1,16 +1,11 @@
 <template>
   <table>
     <tr v-if="headers">
-      <th
-          v-for="header
-          in headers"
-          :key="header.value"
-          :class="`text-${header.align}`"
-          @click="sortCol(header.value, items)">
+      <th v-for="header in headers" :key="header.value" :class="`text-${header.align}`" class="cursor-pointer" @click="sort(header.value)">
         {{ header.text }}
       </th>
     </tr>
-    <tr v-for="(item, itemKey) in items" :key="itemKey">
+    <tr v-for="(item, itemKey) in sortItems(filterItems())" :key="itemKey">
       <td v-for="header in headers" :key="header.value" :class="`text-${header.align}`">
         {{ item[header.value] }}
       </td>
@@ -19,36 +14,47 @@
 </template>
 
 <script setup>
-import {computed, ref} from 'vue';
+import { ref } from 'vue';
 
-defineProps({
+const props = defineProps({
+  filter: {
+    type: String,
+    default: '',
+  },
   headers: {
-    type: Object,
+    type: Array,
     default: null,
   },
   items: {
-    type: Object,
+    type: Array,
     default: null,
     required: true,
   },
 });
 
+const sortedHeader = ref(props.headers[0].value);
 const sortAsc = ref(true);
-const currentSort = ref('name');
 
 const sort = (header) => {
-  if (header === this.currentSort) {
-    this.sortAsc = !this.sortAsc;
+  if (header === sortedHeader.value) {
+    sortAsc.value = !sortAsc.value;
+  } else {
+    sortAsc.value = true;
+    sortedHeader.value = header;
   }
-  this.sortAsc = true;
-  this.currentSort = header;
 };
 
-const sortCol = computed(() => this.items.sort((a, b) => {
-  let modifier = 1;
-  if (!this.sortAsc) modifier = -1;
-  if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
-  if (a[this.currentSort] > b[this.currentSort]) return modifier;
-  return 0;
-}),);
+const sortItems = (items) => {
+  if (!sortedHeader.value) return items;
+  return items.sort((a, b) => {
+    if (sortAsc.value) return a[sortedHeader.value] < b[sortedHeader.value] ? -1 : 1;
+    if (!sortAsc.value) return a[sortedHeader.value] > b[sortedHeader.value] ? -1 : 1;
+    return 0;
+  });
+};
+
+const filterItems = () => {
+  if (!props.filter || props.filter === '') return props.items;
+  return props.items.filter((item) => Object.values(item).join().toLowerCase().includes(props.filter.toLowerCase()));
+};
 </script>
