62ab948d11a27ea98552e01bacfe8aa220e68c26
Florian Berrotµ <florian.berrot@ynov.com>
2022-06-02 09:23:41 +0200

    fix: refacto and delete useless comments

--- src/components/QDragAndDrop/QDragAndDrop.vue
+++ src/components/QDragAndDrop/QDragAndDrop.vue
@@ -1,30 +1,11 @@
 <template>
-<!--  <div id="app">-->
-<!--    <div class="list">-->
-<!--      <transition-group name="flip-list" tag="div">-->
-<!--        <li-->
-<!--          @dragover="(e) => onDragOver(item, i, e)"-->
-<!--          @dragend="(e) => finishDrag(item, i, e)"-->
-<!--          @dragstart="(e) => startDrag(item, i, e)"-->
-<!--          v-for="(item, i) in items"-->
-<!--          class="item"-->
-<!--          :class="{over: (item === over.item && item !== dragFrom),-->
-<!--          [over.dir]: (item === over.item && item !== dragFrom) }"-->
-<!--          draggable="true"-->
-<!--          :key="item"-->
-<!--          :style="{ backgroundColor: backgroundColor }"-->
-<!--        >{{item.title}}-->
-<!--        </li>-->
-<!--      </transition-group>-->
-<!--    </div>-->
-<!--  </div>-->
   <div class="lists">
     <div class="list">
       <ul>
         <li
             class="list-item"
             draggable="true"
-            v-for="(item, i) in items"
+            v-for="(item) in items"
         >{{item.title}}</li>
       </ul>
     </div>
@@ -35,9 +16,6 @@
 </template>
 
 <script>
-
-import {list} from "postcss";
-
 export default {
   name: 'QDragAndDrop',
   components: {},
@@ -49,11 +27,6 @@ export default {
   data() {
     return {
       items: this.item,
-      // backgroundColor: this.color,
-      // over: {},
-      // startLoc: 0,
-      // dragging: false,
-      // dragFrom: {},
     };
   },
   mounted() {
@@ -61,14 +34,11 @@ export default {
   },
   methods: {
     dragAndDrop(){
-      const listItems = document.querySelectorAll(".list-item");
-      const lists = document.querySelectorAll(".list");
+      const listItems = document.getElementsByClassName("list-item");
+      const columns = document.getElementsByClassName("list");
 
       let draggedItem = null;
-
-      for (let i = 0; i < listItems.length; i++) {
-        const item = listItems[i];
-
+      for (let item of listItems) {
         item.addEventListener("dragstart", function () {
           draggedItem = item;
           setTimeout(() => {
@@ -83,129 +53,33 @@ export default {
           }, 0);
         });
 
-        for (let j = 0; j < lists.length; j++) {
-          const list = lists[j];
-
-          list.addEventListener("dragover", function (e) {
+        for (let column of columns) {
+          column.addEventListener("dragover", function (e) {
             e.preventDefault();
           });
 
-          list.addEventListener("dragenter", function (e) {
+          column.addEventListener("dragenter", function (e) {
             e.preventDefault();
             this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
           });
 
-          list.addEventListener("dragleave", function (e) {
+          column.addEventListener("dragleave", function (e) {
             e.preventDefault();
             this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
           });
 
-          list.addEventListener("drop", function (e) {
+          column.addEventListener("drop", function (e) {
             this.append(draggedItem);
             this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
           });
         }
       }
     },
-    //NEW DRAG AND DROP
-    // async dragStart(item){
-    //   await item.addEventListener("dragstart", function () {
-    //     this.draggedItem = item;
-    //     setTimeout(() => {
-    //       item.style.display = "none";
-    //     }, 0);
-    //   });
-    // },
-    // async dragEnd(item){
-    //   await item.addEventListener("dragend", function () {
-    //     setTimeout(() => {
-    //       this.draggedItem.style.display = "block";
-    //       // this.draggedItem = null;
-    //     }, 0);
-    //   });
-    // },
-    // async dragOver(list) {
-    //   await list.addEventListener("dragover", function (e) {
-    //     e.preventDefault();
-    //   });
-    // },
-    // async dragEnter(list){
-    //   await list.addEventListener("dragenter", function (e) {
-    //     e.preventDefault();
-    //     this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
-    //   });
-    // },
-    // async dragLeave(list){
-    //   await list.addEventListener("dragleave", function (e) {
-    //     e.preventDefault();
-    //     this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-    //   });
-    // },
-    // async drop(list, draggedItem){
-    //   await list.addEventListener("drop", function (e) {
-    //     this.append(draggedItem);
-    //     this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-    //   });
-    // }
-    //LAST DRAG AND DROP
-    //   startDrag(item, i, e) {
-    //     this.startLoc = e.clientY;
-    //     this.dragging = true;
-    //     this.dragFrom = item;
-    //   },
-    //   finishDrag(item, pos) {
-    //     this.items.splice(pos, 1);
-    //     this.items.splice(this.over.pos, 0, item);
-    //     this.over = {};
-    //   },
-    //   onDragOver(item, pos, e) {
-    //     const dir = (this.startLoc < e.clientY) ? 'down' : 'up';
-    //     setTimeout(() => {
-    //       this.over = { item, pos, dir };
-    //     }, 50);
-    //   },
-    // },
   }
 };
 </script>
 
 <style>
-/*LAST DRAG AND DROP*/
-/*.list > div {*/
-/*  display: flex;*/
-/*  flex-direction: column;*/
-/*}*/
-
-/*.item {*/
-/*  width: 180px;*/
-/*  padding: 10px;*/
-/*  margin: 10px auto 10px 10px;*/
-/*  color: white;*/
-/*  font-family: sans-serif;*/
-/*  border-radius: 4px;*/
-/*  display: inline-block;*/
-/*  position: relative;*/
-/*  box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);*/
-/*  cursor: grab;*/
-/*}*/
-
-/*.item:hover{*/
-/*  opacity: 0.7;*/
-/*}*/
-
-/*.flip-list-move {*/
-/*  transition: transform .2s;*/
-/*}*/
-
-/*.over {*/
-/*  opacity: .6;*/
-/*}*/
-/*NEW DRAG AND DROP*/
-body {
-  font-family: sans-serif;
-  background-color: #ffce00;
-}
-
 * {
   margin: 0;
   padding: 0;
