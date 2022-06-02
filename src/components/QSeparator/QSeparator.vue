5fd267e6aacf53c42ff48ec3333393c6222f00e3
Guillaume Chx <guillaume.chateauroux@gmail.com>
2022-06-02 10:11:06 +0200

    Added: QSeparator component

---
+++ src/components/QSeparator/QSeparator.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="separatorClasses" :style="separatorStyles" />
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  vertical: {
+    type: Boolean,
+    default: false,
+  },
+  color: {
+    type: String,
+    default: '#000000',
+  },
+  size: {
+    type: String,
+    default: '1px',
+  },
+});
+
+const separatorClasses = computed(() => (props.vertical ? 'h-full' : 'w-full'));
+const separatorStyles = computed(() => {
+  const styles = [];
+
+  styles.push(`background-color: ${props.color}`);
+
+  if (props.vertical) {
+    styles.push(`width: ${props.size}`);
+  } else {
+    styles.push(`height: ${props.size}`);
+  }
+
+  return styles.join('; ');
+});
+</script>
