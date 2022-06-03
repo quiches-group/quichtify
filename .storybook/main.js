7723e62fea5a3f80a1712e6b71495b4ae5719c75
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2022-06-03 15:16:37 +0200

    chrore: add tw and sass support for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,21 @@
 module.exports = {
   stories: ['./*.stories.@(js|jsx|ts|tsx)', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    {
+      name: 'storybook-addon-sass-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+        sassLoaderOptions: {
+          implementation: require('sass'),
+        },
+      },
+    },
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: '@storybook/vue3',
   core: {
     builder: '@storybook/builder-vite',
