387d65dbff5eb54ebb4df3b7e29bb25bc63b3916
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2022-06-05 16:28:21 +0200

    fix: bad code code quality by disabling eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'airbnb-base',
-    'plugin:storybook/recommended',
-    'prettier',
-  ],
+  extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'plugin:storybook/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'vue',
-  ],
+  plugins: ['vue'],
   rules: {
     'vue/no-multiple-template-root': 'off',
     'vue/require-default-prop': 'off',
--- src/components/QDragAndDrop/QDragAndDrop.vue
+++ src/components/QDragAndDrop/QDragAndDrop.vue
@@ -1,19 +1,9 @@
 <template>
-  <div class="flex w-full min-w-250 max-w-350 h-full p-8 rounded lists">
-    <div
-        v-for="(column, c) in columns"
-        :key="c"
-        class="columns flex w-full min-w-250 max-w-350 h-full min-h-150 p-8 rounded"
-    >
-      <p class="p-4 m-0 box-border">{{column.title}}</p>
+  <div class="flex p-8 w-full h-full rounded min-w-250 max-w-350 lists">
+    <div v-for="(column, c) in columns" :key="c" class="flex p-8 w-full h-full rounded columns min-w-250 max-w-350 min-h-150">
+      <p class="box-border p-4 m-0">{{ column.title }}</p>
       <ul>
-        <li
-            v-for="(item, i) in column.items"
-            :key="i"
-            class="list-item my-2 bg-white px-4 py-5 text-center"
-            draggable="true"
-            :style="{ backgroundColor: color }"
-        >{{item.title}}</li>
+        <li v-for="(item, i) in column.items" :key="i" class="list-item px-4 py-5 my-2 text-center bg-white" draggable="true" :style="{ backgroundColor: color }">{{ item.title }}</li>
       </ul>
     </div>
   </div>
@@ -21,31 +11,29 @@
 
 <script>
 export default {
+  name: 'QDragAndDrop',
   props: {
     columnsOfItems: {
       type: Array,
       required: true,
-      default: () => ([
-        { id: 1, title: 'TODO', items: [
-            {id: 1, title: 'Titre 1'},
-            {id: 2, title: 'Titre 2'},
-          ]
+      default: () => [
+        {
+          id: 1,
+          title: 'TODO',
+          items: [
+            { id: 1, title: 'Titre 1' },
+            { id: 2, title: 'Titre 2' },
+          ],
         },
-        { id: 2, title: 'IN PROGRESS', items: [
-            {id: 3, title: 'Titre 3'},
-          ]
-        },
-        { id: 3, title: 'DONE', items: [
-            {id: 4, title: 'Titre 4'},
-          ]
-        },
-      ]),
+        { id: 2, title: 'IN PROGRESS', items: [{ id: 3, title: 'Titre 3' }] },
+        { id: 3, title: 'DONE', items: [{ id: 4, title: 'Titre 4' }] },
+      ],
     },
     color: {
       type: String,
       required: false,
-      default: '#fff'
-    }
+      default: '#fff',
+    },
   },
   data() {
     return {
@@ -56,54 +44,53 @@ export default {
     this.dragAndDrop();
   },
   methods: {
-    dragAndDrop () {
-      const listItems = document.getElementsByClassName("list-item");
-      const columns = document.getElementsByClassName("columns");
+    dragAndDrop() {
+      const listItems = document.getElementsByClassName('list-item');
+      const columns = document.getElementsByClassName('columns');
 
       let draggedItem = null;
       for (const item of listItems) {
-        item.addEventListener("dragstart", () => {
+        item.addEventListener('dragstart', () => {
           draggedItem = item;
           setTimeout(() => {
-            item.style.display = "none";
+            item.style.display = 'none';
           }, 0);
         });
 
-        item.addEventListener("dragend", () => {
+        item.addEventListener('dragend', () => {
           setTimeout(() => {
-            draggedItem.style.display = "block";
+            draggedItem.style.display = 'block';
             draggedItem = null;
           }, 0);
         });
 
         for (const column of columns) {
-          column.addEventListener("dragover", (e) => {
+          column.addEventListener('dragover', (e) => {
             e.preventDefault();
           });
 
-          column.addEventListener("dragenter", function (e) {
+          column.addEventListener('dragenter', function (e) {
             e.preventDefault();
-            this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+            this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
           });
 
-          column.addEventListener("dragleave", function (e) {
+          column.addEventListener('dragleave', function (e) {
             e.preventDefault();
-            this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+            this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
           });
 
-          column.addEventListener("drop", function () {
+          column.addEventListener('drop', function () {
             this.append(draggedItem);
-            this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+            this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
           });
         }
       }
     },
-  }
+  },
 };
 </script>
 
 <style scoped>
-
 .lists .columns {
   flex-flow: column;
   background-color: rgba(0, 0, 0, 0.1);
