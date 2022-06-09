b216afeb25e69714090d098d3f6bc9941abda8a5
Laroche-Nicolas <larochenicolas.info@gmail.com>
2022-06-10 00:08:01 +0200

    Update style and personalisation

--- src/components/QSnackbar/Example.vue
+++ src/components/QSnackbar/Example.vue
@@ -1,19 +1,22 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <q-snackbar :is-open="state.showSnackbar">
+  <q-snackbar :is-open="state.showSnackbar" size="medium" position="bottom-left" color="red">
+    <div class="flex flex-col">
     <strong class="font-bold">Holy smokes!</strong>
     <span class="block sm:inline">Something seriously bad happened.</span>
     <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-          <button @click="buttonAction(false)">Close</button>
+    <button @click="buttonAction(false)">Close</button>
   </span>
+    </div>
   </q-snackbar>
   <div>
     <button @click="buttonAction(true)">Open Snackbar</button>
   </div>
+
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import {reactive} from 'vue';
 
 const state = reactive({
   showSnackbar: false,
--- src/components/QSnackbar/QSnackbar.vue
+++ src/components/QSnackbar/QSnackbar.vue
@@ -1,25 +1,113 @@
 <template>
-  <div
-      v-if="isOpen"
-      class="border px-4 py-3 rounded relative" role="alert"
-      :class="{ 'animate-fadeIn': isOpen, 'animate-fadeOut': isOpen === false, 'opacity-background': isOpen}"
-      >
-    <slot />
+  <div class="h-32 w-32">
+    <div v-if="isOpen" class="z-50 border px-4 py-3 rounded" :class="classes" :style="`background-color: ${color}`">
+      <slot/>
+    </div>
   </div>
 </template>
 
 <script setup>
+import {computed} from "vue";
 
-defineProps({
+const props = defineProps({
   isOpen: {
     type: Boolean,
     required: true,
   },
+  size: {
+    type: String,
+    default: 'medium',
+    validator(value) {
+      const validValues = ['small', 'medium', 'large'];
+      return validValues.includes(value);
+    },
+  },
+  position: {
+    type: String,
+    default: 'bottom',
+    validator(value) {
+      const validValues = ['bottom', 'bottom-left', 'bottom-right', 'top', 'top-left', 'top-right'];
+      return validValues.includes(value);
+    },
+  },color: {
+    type: String,
+    default: 'rgba(0, 0, 0, 0.1)',
+  },
 });
+console.log(props.isOpen)
+const classes = computed(() => {
+  const styles = [];
 
+  if (props.isOpen) {
+    styles.push('q-animate-fadeIn');
+  }
+ // Size
+  if (props.size === 'small') {
+    styles.push('q-snkb--small');
+  }
 
+  if (props.size === 'medium') {
+    styles.push('q-snkb--medium');
+  }
+
+  if (props.size === 'large') {
+    styles.push('q-snkb--large');
+  }
+  // Position
+  if (props.position === 'bottom') {
+    styles.push('q-snkb--bottom')
+  }
+
+  if (props.position === 'bottom-left') {
+    styles.push('q-snkb--bottom-left')
+  }
+
+  if (props.position === 'bottom-right') {
+    styles.push('q-snkb--bottom-right')
+  }
+
+  if (props.position === 'top') {
+    styles.push('q-snkb--top')
+  }
+  if (props.position === 'top-left') {
+    styles.push('q-snkb--top-left')
+  }
+  if (props.position === 'top-right') {
+    styles.push('q-snkb--top-right')
+  }
+  return styles;
+})
 </script>
 
 <style scoped>
 
+/* Size */
+.q-snkb--small {
+  @apply w-3/12
+}
+.q-snkb--medium {
+  @apply w-6/12
+}.q-snkb--large {
+  @apply w-9/12
+}
+
+/* Position */
+.q-snkb--bottom {
+  @apply absolute left-1/2 -translate-x-1/2 bottom-2
+}
+.q-snkb--bottom-left {
+  @apply absolute left-2 bottom-2
+}
+.q-snkb--bottom-right {
+  @apply absolute right-2 bottom-2
+}
+.q-snkb--top {
+  @apply absolute left-1/2 -translate-x-1/2 top-2
+}
+.q-snkb--top-right {
+  @apply absolute right-2 top-2
+}
+.q-snkb--top-left {
+  @apply absolute left-2 top-2
+}
 </style>
