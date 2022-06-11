b61ff9be4ce832a7f29349d3d611f75ffc62085f
Maxence Mottard <maxence.mottard@worldline.com>
2022-06-11 11:14:03 +0200

    Revert "fix eslint"
    This reverts commit f6a7b3fc7b79f2e0bbb972a414d6eb25fdf2ab5b.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'eslint:recommended', '@vue/prettier'],
+  extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'plugin:storybook/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
